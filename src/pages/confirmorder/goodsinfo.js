2f7ff94a0ce59722adcd3c45619fca3e2edc9c1f
hanzhecheng <1119216543@qq.com>
2019-02-19 14:46:57 +0800

    增加确认订单页面

---
+++ src/pages/confirmorder/goodsinfo.js
@@ -0,0 +1,31 @@
+import Taro from '@tarojs/taro';
+import { View, Image } from '@tarojs/components';
+import './index.scss'
+export default class GoodsInfo extends Taro.Component {
+    render() {
+        const { shopname, list } = this.props.data
+        return (
+            <View className="confirmorder__goods">
+                <View className="confirmorder__goods__shopname">
+                    {shopname}
+                </View>
+                {list.map((item, index) => {
+                    return (
+                        <View className="at-row at-row--wrap" key={index}>
+                            <View className='at-col at-col-3'>
+                                <Image src={item.image} className="confirmorder__goods__img"></Image>
+                            </View>
+                            <View className='at-col at-col-9'>
+                                <View className="confirmorder__goods__label">商品名称:{item.name}</View>
+                                <View className="confirmorder__goods__label">商品描述:{item.desc}</View>
+                                <View className="confirmorder__goods__label">商品规格:{item.unit}</View>
+                                <View className="confirmorder__goods__label">商品价格:{item.price}</View>
+                            </View>
+                        </View>
+                    )
+                })}
+
+            </View>
+        );
+    }
+}
---
+++ src/pages/confirmorder/index.js
@@ -0,0 +1,149 @@
+import Taro from '@tarojs/taro';
+import { View, Text } from '@tarojs/components';
+import GoodsInfo from './goodsinfo'
+import './index.scss'
+export default class Index extends Taro.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            amount:0,
+            goodsList: [
+                {
+                    shopname: "MATRO GBJ 珠宝旗舰店",
+                    list: [
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1547188839089.png',
+                            name: '奶粉',
+                            unit: '瓶',
+                            desc: '高级奶粉，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        },
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1547188839089.png',
+                            name: '奶粉',
+                            unit: '瓶',
+                            desc: '高级奶粉，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        },
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1547188839089.png',
+                            name: '奶粉',
+                            unit: '瓶',
+                            desc: '高级奶粉，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        },
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1547188839089.png',
+                            name: '奶粉',
+                            unit: '瓶',
+                            desc: '高级奶粉，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        },
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1547188839089.png',
+                            name: '奶粉',
+                            unit: '瓶',
+                            desc: '高级奶粉，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        }
+                    ]
+                },
+                {
+                    shopname: "食甲天下",
+                    list: [
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1546934715988.jpg?x-image-process=image/resize,w_220',
+                            name: '夹心饼',
+                            unit: '瓶',
+                            desc: '高级夹心饼，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        },
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1546934715988.jpg?x-image-process=image/resize,w_220',
+                            name: '夹心饼',
+                            unit: '瓶',
+                            desc: '高级夹心饼，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        },
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1546934715988.jpg?x-image-process=image/resize,w_220',
+                            name: '夹心饼',
+                            unit: '瓶',
+                            desc: '高级夹心饼，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        },
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1546934715988.jpg?x-image-process=image/resize,w_220',
+                            name: '夹心饼',
+                            unit: '瓶',
+                            desc: '高级夹心饼，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        },
+                        {
+                            image: 'http://img.hbunion.com/upload/image/201901/1546934715988.jpg?x-image-process=image/resize,w_220',
+                            name: '夹心饼',
+                            unit: '瓶',
+                            desc: '高级夹心饼，品质好，价格低',
+                            price: '12000',
+                            isOpened: false,
+                            num: 1
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    config = {
+        navigationBarTitleText: '确认订单'
+    }
+    onConfirm = () => {
+
+    }
+    render() {
+        return (
+            <View className="confirmorder">
+                <View className="confirmorder__address">
+                    <Text className="confirmorder__address__title">收货信息</Text>
+                    <View className='at-row at-row--wrap'>
+                        <View className='at-col at-col-4 confirmorder__address__name'>韩喆成</View>
+                        <View className='at-col at-col-8 confirmorder__address__phone'>156****1281</View>
+                        <View className='at-col at-col-12 confirmorder__address__detail'>江苏/苏州市/姑苏区/阔巷22号</View>
+                    </View>
+                </View>
+                {this.state.goodsList.map((item, index) => {
+                    return (
+                        <GoodsInfo data={item} key={index}></GoodsInfo>
+                    )
+                })}
+
+                <View className="confirmorder__amount">
+                    <View className='at-row at-row--wrap'>
+                        <View className='at-col at-col-2 confirmorder__amount__title'>应付金额:</View>
+                        <View className='at-col at-col-7 confirmorder__amount__real'>{this.state.amount}</View>
+                        <View className='at-col at-col-3 '>
+                            <Button className="confirmorder__amount__btn" onClick={this.onConfirm}>确认订单</Button>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
