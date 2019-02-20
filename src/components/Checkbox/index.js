b8f5641e8024882ba4bdbc58b95a08c9b8dbbfb1
hanzhecheng <1119216543@qq.com>
2019-02-20 13:30:13 +0800

    checkbox组件增加size属性设置

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -2,14 +2,14 @@ import Taro from '@tarojs/taro';
 import { View } from '@tarojs/components';
 import './index.scss'
 export default class Checkbox extends Taro.Component {
-    
+
     onClick = () => {
         this.props.onChange(!this.props.checked)
     }
     render() {
         return (
-            <View className="hb__checkbox__area" onClick={this.onClick}>
-                <View className={`hb__checkbox ${this.props.checked ? 'hb__checkbox--check' : ''}`} >
+            <View className='hb__checkbox__area' onClick={this.onClick}>
+                <View className={`hb__checkbox ${this.props.checked ? 'hb__checkbox--check' : ''} ${this.props.size ? 'hb__checkbox__area--' + this.props.size : ''}`} >
                 </View>
                 <View className="hb__checkbox__label">{this.props.label ? this.props.label : ''}</View>
             </View>
